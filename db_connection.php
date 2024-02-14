e4ffa8dace5cbbb74b84ffb44d10d64b99035035
ravindutha <152910562+ravindutha@users.noreply.github.com>
2024-02-14 12:10:51 +0530

    create database and connect

---
+++ db_connection.php
@@ -0,0 +1,10 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "library_system";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
