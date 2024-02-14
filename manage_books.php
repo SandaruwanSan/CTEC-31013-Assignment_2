d91954c76b078b4e5ac3837b5b1ef3076d3b5113
athapat-ct19038 <147899798+athapat-ct19038@users.noreply.github.com>
2024-02-14 13:56:50 +0530

    html table

---
+++ manage_books.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Books</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2 class="my-4">Manage Books</h2>
+      
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Book ID</th>
+                    <th>Book Name</th>
+                    <th>Category</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    </div>
+    <form method="post" action="">
+            <input type="submit" name="logout" value="Logout" class="btn btn-danger mb-3"><br><a href="admin.php" class="btn btn-secondary">Admin Page</a><p>   </p><a href="listallbooks.php" class="btn btn-secondary">Book List Page</a>
+        </form>
+</body>
+</html>
